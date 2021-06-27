1041073905785872e77d8d608c52ce8f561ed79b
Shubham bagade <72594576+MrShubham1267@users.noreply.github.com>
2021-06-27 21:56:56 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -14,27 +14,27 @@ model = joblib.load(filename)
 
 #st.write(str(model))
                             
-season = st.selectbox('Season (1:spring, 2:summer, 3:fall, 4:winter)',["1","2","3","4"])
+season = st.selectbox('Season (1:spring, 2:summer, 3:fall, 4:wfloater)',["1","2","3","4"])
 
 yr = st.selectbox('Year (0: 2018, 1:2019)',["0","1"])
 
-mnth = st.text_input(label=' month (1 to 12)')
+mnth = st.text_input(label=' month (1 to 12)',value=1)
 
 weekday = st.selectbox('Day of the week',["0","1","2","3","4","5","6"])
 
 weathersit = st.selectbox('weathersit',["1","2","3"])
 
-temp = st.text_input(label='Temperature in Celsius')
+temp = st.text_input(label='Temperature in Celsius',value=0)
 
-atemp = st.text_input(label='Feeling temperature in Celsius')
+atemp = st.text_input(label='Feeling temperature in Celsius',value=0)
 
-hum = st.text_input(label='Humidity')
+hum = st.text_input(label='Humidity',value=0)
 
-windspeed = st.text_input(label='Wind speed')
+windspeed = st.text_input(label='Wind speed',value=0)
 
-casual = st.text_input(label='Count of casual users')
+casual = st.text_input(label='Count of casual users',value=0)
 
-registered =st.text_input(label='Count of registered users')
+registered =st.text_input(label='Count of registered users',value=0)
 
 
 pred = model.predict([[int(season),int(yr),int(mnth),int(weekday),int(weathersit),int(temp),int(atemp),int(hum),int(windspeed),int(casual),int(registered)]])
