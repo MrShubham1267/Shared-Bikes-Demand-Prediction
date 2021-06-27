d0788f8ce646940b586cf4d49cb54b4755523ed6
Shubham bagade <72594576+MrShubham1267@users.noreply.github.com>
2021-06-27 21:54:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,23 +18,23 @@ season = st.selectbox('Season (1:spring, 2:summer, 3:fall, 4:winter)',["1","2","
 
 yr = st.selectbox('Year (0: 2018, 1:2019)',["0","1"])
 
-mnth = st.text_input(label=' month (1 to 12)',value=1)
+mnth = st.text_input(label=' month (1 to 12)')
 
 weekday = st.selectbox('Day of the week',["0","1","2","3","4","5","6"])
 
 weathersit = st.selectbox('weathersit',["1","2","3"])
 
-temp = st.text_input(label='Temperature in Celsius',value=0)
+temp = st.text_input(label='Temperature in Celsius')
 
-atemp = st.text_input(label='Feeling temperature in Celsius',value=0)
+atemp = st.text_input(label='Feeling temperature in Celsius')
 
-hum = st.text_input(label='Humidity',value=0)
+hum = st.text_input(label='Humidity')
 
-windspeed = st.text_input(label='Wind speed',value=0)
+windspeed = st.text_input(label='Wind speed')
 
-casual = st.text_input(label='Count of casual users',value=0)
+casual = st.text_input(label='Count of casual users')
 
-registered =st.text_input(label='Count of registered users',value=0)
+registered =st.text_input(label='Count of registered users')
 
 
 pred = model.predict([[int(season),int(yr),int(mnth),int(weekday),int(weathersit),int(temp),int(atemp),int(hum),int(windspeed),int(casual),int(registered)]])
